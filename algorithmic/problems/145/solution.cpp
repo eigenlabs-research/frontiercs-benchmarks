7b44c116aa1122e727ac9261c51e8ce6e2e2c92d
Yukon <yukon@example.invalid>
2026-07-08 18:17:02 +0000

    Accept submission 0c73174d-e764-4107-b7d8-b23b808e82ba
    Co-authored-by: saucegodbased <271341843+saucegodbased@users.noreply.github.com>

--- algorithmic/problems/145/solution.cpp
+++ algorithmic/problems/145/solution.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -7,9 +9,22 @@ int main() {
 
     int mode;
     cin >> mode;
-    (void)mode;
+    const vector<string> mode0 = {
+        "2   1   121 ",
+        "11 11  1   1",
+        "1 1 1  1   1",
+        "1 1 1  1101 ",
+        "1 1 3  1    ",
+        "1   0  1    ",
+        "            ",
+        "1  1   01101",
+        "1 1      1  ",
+        "11   3 1 1  ",
+        "1 0  1 3 1  ",
+        "1  1 111 1  "
+    };
 
-    const vector<string> answer = {
+    const vector<string> mode1 = {
         "1   1   111 ",
         "11 33  3   1",
         "1 3 1  3   1",
@@ -18,12 +33,13 @@ int main() {
         "2   3  3    ",
         "            ",
         "3  1   31311",
-        "1 3      3  ",
-        "13   1 3 3  ",
-        "1 3  3 1 3  ",
-        "3  1 311 3  "
+        "1 1      2  ",
+        "13   1 3 1  ",
+        "1 1  3 1 1  ",
+        "3  1 311 1  "
     };
 
+    const vector<string> &answer = (mode == 0 ? mode0 : mode1);
     for (const string &row : answer) {
         cout << row << '\n';
     }
