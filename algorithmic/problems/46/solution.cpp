10a846f47fa7dee9e3086be8482cdb33b57b7694
Yukon <yukon@example.invalid>
2026-07-09 09:50:33 +0000

    Accept submission e0253165-3040-4bbd-8f33-694e78427e1e
    Co-authored-by: rainbowpuffpuff <83903147+rainbowpuffpuff@users.noreply.github.com>

--- algorithmic/problems/46/solution.cpp
+++ algorithmic/problems/46/solution.cpp
@@ -345,7 +345,7 @@ static void collectTabu(const vector<vector<int>>& cur, const Mv& mv){
 
 int main(){
     auto T0 = chrono::steady_clock::now();
-    const auto budget = chrono::milliseconds(860);
+    const auto budget = chrono::milliseconds(940); // use more of the 1s TL
 
     if(scanf("%d %d", &J, &M) != 2) return 0;
     N = J*M;
